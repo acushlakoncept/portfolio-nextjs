d6651dc7fe9d2f7902a1969ac70675b788709b4a
Uduak Essien <acushla4real@gmail.com>
2022-03-07 19:01:57 +0100

    Add footer component

---
+++ components/footer/index.js
@@ -0,0 +1,18 @@
+import SocialLinks from "../../constants/socialLinks"
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>
+        <SocialLinks styleClass="footer-links"></SocialLinks>
+        <h4>
+          copyright&copy; {new Date().getFullYear()}
+          <span> UduakEssien</span> all rights reserved
+        </h4>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
